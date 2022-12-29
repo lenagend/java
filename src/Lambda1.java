4b3ab47767d31cb56b6f18084bd271b0dd758b4c
lenagend <57205046+lenagend@users.noreply.github.com>
2022-12-29 14:19:01 +0900

    Initial commit

---
+++ src/Lambda1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lambda1 {
+    public static void main(String[] args) {
+
+
+        String name = "KimKwangmin";
+        String name2 = "ParkSoHyun";
+        List<String> nameList = new ArrayList<String>();
+        nameList.add(name);
+        nameList.add(name2);
+        Collections.sort(nameList, (a, b)-> Integer.compare(a.length(), b.length()));
+
+        for (int i = 0; i < nameList.size(); i++){
+            System.out.println(nameList.get(i));
+        }
+
+    }
+
+}
