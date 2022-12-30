0b5e4464b53c9d5203c5206f715ee60c93fbb2f6
lenagend <57205046+lenagend@users.noreply.github.com>
2022-12-30 20:44:30 +0900

    20221230

---
+++ src/FunctionTraining.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionTraining {
+    public static void main(String[] args) {
+        List<String> strs = convert(Arrays.asList(1,2,3,4,5), (Integer i)->i+"번");
+        for(String s: strs){
+            System.out.println(s);
+        }
+    }
+    public static  <T, R> List<R> convert(List<T> list, Function<T, R> f){
+        List<R> result = new ArrayList<>();
+        for(T t: list){
+            result.add(f.apply(t));
+        }
+        return result;
+    }
+}
